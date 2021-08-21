58d1f03a50449150fbdeed83ddf61e215e512147
alecmocatta <alec@mocatta.net>
2021-08-21 15:27:46 +0100

    add fn apply_options to OptionalStruct impl

--- src/lib.rs
+++ src/lib.rs
@@ -167,7 +167,7 @@ fn parse_attributes(ast: &syn::DeriveInput) -> Data {
 
 fn create_struct(fields: &Vec<Field>, data: Data, generics: &Generics) -> Tokens {
     let (orignal_struct_name, optional_struct_name, derives, nested_names) = data.explode();
-    let (assigners, attributes, empty) = create_fields(&fields, nested_names);
+    let (assigners, optional_assigners, attributes, empty) = create_fields(&fields, nested_names);
 
     let (_, generics_no_where, _) = generics.split_for_impl();
 
@@ -184,6 +184,12 @@ fn create_struct(fields: &Vec<Field>, data: Data, generics: &Generics) -> Tokens
         }
 
         impl #generics #optional_struct_name #generics_no_where {
+            pub fn apply_options(&mut self, optional_struct: #optional_struct_name #generics_no_where) {
+                #optional_assigners
+            }
+        }
+
+        impl #generics #optional_struct_name #generics_no_where {
             pub fn empty() -> #optional_struct_name #generics_no_where {
                 #optional_struct_name {
                     #empty
@@ -196,15 +202,17 @@ fn create_struct(fields: &Vec<Field>, data: Data, generics: &Generics) -> Tokens
 fn create_fields(
     fields: &Vec<Field>,
     nested_names: HashMap<String, String>,
-) -> (Tokens, Tokens, Tokens) {
+) -> (Tokens, Tokens, Tokens, Tokens) {
     let mut attributes = quote!{};
     let mut assigners = quote!{};
+    let mut optional_assigners = quote!{};
     let mut empty = quote!{};
     for field in fields {
         let ref type_name = &field.ty;
         let ref field_name = &field.ident.clone().unwrap();
         let next_attribute;
         let next_assigner;
+        let next_optional_assigner;
         let next_empty;
 
         let type_name_string = quote!{#type_name}.to_string();
@@ -213,11 +221,13 @@ fn create_fields(
         if type_name_string.starts_with("Option<") {
             next_attribute = quote!{ pub #field_name: #type_name, };
             next_assigner = quote!{ self.#field_name = optional_struct.#field_name; };
+            next_optional_assigner = quote!{ self.#field_name = optional_struct.#field_name; };
             next_empty = quote!{ #field_name: None, };
         } else if nested_names.contains_key(&type_name_string) {
             let type_name = Ident::new(nested_names.get(&type_name_string).unwrap().as_str());
             next_attribute = quote!{ pub #field_name: #type_name, };
             next_assigner = quote!{ self.#field_name.apply_options(optional_struct.#field_name); };
+            next_optional_assigner = quote!{ self.#field_name.apply_options(optional_struct.#field_name); };
             next_empty = quote!{ #field_name: #type_name::empty(), };
         } else {
             next_attribute = quote! { pub #field_name: Option<#type_name>, };
@@ -226,13 +236,19 @@ fn create_fields(
                     self.#field_name = attribute;
                 }
             };
+            next_optional_assigner = quote!{
+                if let Some(attribute) = optional_struct.#field_name {
+                    self.#field_name = Some(attribute);
+                }
+            };
             next_empty = quote!{ #field_name: None, };
         }
 
         assigners = quote!{ #assigners #next_assigner };
+        optional_assigners = quote!{ #optional_assigners #next_optional_assigner };
         attributes = quote!{ #attributes #next_attribute };
         empty = quote!{ #empty #next_empty }
     }
 
-    (assigners, attributes, empty)
+    (assigners, optional_assigners, attributes, empty)
 }
